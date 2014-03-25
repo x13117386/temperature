40688180c7fa97909e6afa55ae6b780fe62e254d
jhugh <jhugh@github.com>
2014-03-25 09:51:42 +0000

    updated test

--- test/spec/converterSpec.js
+++ test/spec/converterSpec.js
@@ -1,8 +1,14 @@
 describe("My Converter", function() {
-    it("should convert imperial weight to metric", function() {
-        expect(Converter.convertFromPoundsToKIlo(2.2)).toBe(1);
+    it("should convert fahrenheit to celsius", function () {
+        expect(Converter.convertFromFahrToCels(-40)).toBe(-40);
     })
-    it("should be able to deal with strings", function(){
-        expect(function() {Converter.convertFromPoundsToKIlo("hello")}).toThrow(new Error("Not a number"))
+    it("fahrenheit to celsius should be able to deal with strings", function () {
+        expect(function () { Converter.convertFromFahrToCels("hello") }).toThrow(new Error("Not a number"));
+    })
+    it("should convert celsius to fahrenheit", function () {
+        expect(Converter.convertFromCelsToFahr(100)).toBe(212);
+    })
+    it("celsius to fahrenheit should be able to deal with strings", function () {
+        expect(function () { Converter.convertFromCelsToFahr("hello") }).toThrow(new Error("Not a number"));
     })
 })
